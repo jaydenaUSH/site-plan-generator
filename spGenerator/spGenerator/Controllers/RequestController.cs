e647dc009457a002922b8687525c23155d242957
unknown <jayden.aurelus@ushunger.com>
2026-07-01 20:54:56 +0000

    Get by id endpoint

--- spGenerator/spGenerator/Controllers/RequestController.cs
+++ spGenerator/spGenerator/Controllers/RequestController.cs
@@ -1,4 +1,6 @@
 using spGenerator;
+using System.Web.Http;
+using System.Linq;
 
 namespace spGenerator.Controllers {
     [RoutePrefix("api/requests")]
@@ -14,8 +16,16 @@ namespace spGenerator.Controllers {
 
         //Get requests
         [HttpGet]
-        [Route("{id}:int")]
+        [Route("{id:int}")]
         public IHttpActionResult GetReqByID(int id) {
+            var row = _db.SitePlanRequests.Find(id);
+            if (row != null) {
+                return Ok(row);
+            } else
+            {
+                return NotFound();
+            }
+
         }
 
     }

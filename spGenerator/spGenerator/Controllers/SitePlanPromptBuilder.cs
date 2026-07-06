4a03543e2ae8510424cb13a9adc0a8f0424a8a49
unknown <jayden.aurelus@ushunger.com>
2026-07-06 18:28:31 +0000

    Prompt engineering, package folder removed

--- spGenerator/spGenerator/Controllers/SitePlanPromptBuilder.cs
+++ spGenerator/spGenerator/Controllers/SitePlanPromptBuilder.cs
@@ -3,6 +3,10 @@ using System.Security.Policy;
 using System.Text;
 using System.Web.Http;
 using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+using OpenAI;
+using OpenAI.Responses;
 namespace spGenerator.Controllers{
     [RoutePrefix("api/prompt")]
 
@@ -19,26 +23,40 @@ namespace spGenerator.Controllers{
         public string GeneratePrompt(SitePlanRequest req)
         {
             var prompt = new StringBuilder();
-            prompt.Append("Can you generate a site plan that plans how to setup a venue. The details for this venue are provided in a JSON structure.\n");
+            prompt.Append("Can you generate a site plan that plans how to setup a venue. The details for this venue are provided in a JSON structure.\n\n");
             prompt.Append(JsonConvert.SerializeObject(req));
-            prompt.Append("\nPlease use the context from [VECTORDB] with past site plans to generate your new site plan for these details following similar logic as the ones provided from context.");
-            prompt.Append("The response must include sections detailing the site overivew, " +
-                "recommmended layout, volunteer flow, supply flow, timeline, risks, PM Review checklsit.");
+            prompt.Append("\n\nPlease use the context from [VECTORDB] holding past site plans as a basis to understand the logic behind making site plans so " +
+                            "you can generate a new site plan for this sepcific venue given the details.");
+            prompt.Append("The response must include sections detailing the Site Overview, " +
+                            "Recommmended Layout, Volunteer Flow, Supply Flow, Timeline, Risks, PM Review Checklist in the form of JSON.");
 
             return prompt.ToString();
         }
-
-        public void askAI(SitePlanRequest req)
+        [HttpPost]
+        [Route("askAI")]
+        public async  Task<System.ClientModel.ClientResult> askAI(SitePlanRequest req)
         {
             var instructions = GeneratePrompt(req);
 
-            //Insert code for AI API
-            var response = new SitePlanRequest();// 
+            //Code to make AI API instance and write request
+            var client = new OpenAIClient(
+                        Environment.GetEnvironmentVariable("OPENAI_API_KEY")
+                    );
+
+            #pragma warning disable OPENAI001
+            var responseClient = client.GetResponsesClient();
+
+            var answer = await responseClient.CreateResponseAsync(model: "gpt-5.5", userInputText: instructions);
+            //Whatever the api outputs, save as response
+            //return openaiRes;
+
+            return answer;
+
 
             //Save draft to sql
-            _db.SitePlanRequests.Add(response);
             _db.SaveChanges();
             //return draft
+            return null;
 
         }
 

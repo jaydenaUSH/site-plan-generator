7d875e0a51e69d0fb2107ce79f707cdca62b88eb
unknown <jayden.aurelus@ushunger.com>
2026-07-06 19:41:01 +0000

    PM review endpoints for edit drafts)

---
+++ spGenerator/spGenerator/Controllers/ReviewController.cs
@@ -0,0 +1,56 @@
+﻿    using spGenerator;
+    using System;
+    using System.Web.Http;
+
+    namespace spGenerator.Controllers
+    {
+        [RoutePrefix("/api/review")]
+        public class ReviewController : ApiController
+        {
+            private readonly SitePlanAIPOCEntities _db;
+            public ReviewController()
+            {
+                _db = new SitePlanAIPOCEntities(); // Replace  SitePlanAIPOCEntities to SitePlanDraft (placeholder for now)
+        }
+            [HttpGet]
+            [Route("{id:int}/draft")]
+            public IHttpActionResult getSitePlanDraft(int id) {
+            var row = _db.SitePlanRequests.Find(id); // Replace SitePlanRequests to SitePlanDraft (placeholder for now) 
+            if(row!= null) {
+                return Ok(row);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        //edit everything 
+        [HttpPut]
+        [Route("{id:int}/draft/edit")]
+        public IHttpActionResult editDraft(int id, SitePlanRequest req) //Replace all instances of SitePlanRequest to SitePlanDraft
+        {
+            var ogVals = _db.SitePlanRequests.Find(id);
+
+
+            return null;
+        } 
+
+        //Save the submitted structure into the final table 
+        public IHttpActionResult finalizePlan(int id)
+        {
+            try{
+                var row = _db.SitePlanRequests.Find(id); // Replace SitePlanRequests to SitePlanDraft (placeholder for now) 
+                _db.SitePlanRequests.Add(row);// Replace SitePlanRequests to SitePlanFinal (placeholder for now) for all below lines
+                _db.SaveChanges();
+
+
+                return Ok();
+                //Save reviewer, and timestamp
+            }
+            catch (Exception ex) {
+                return InternalServerError(ex);
+            }
+        }
+
+        }
+}

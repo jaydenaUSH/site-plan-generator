aae15b0c62a8a7ab816f1718db7e2720542c912c
unknown <jayden.aurelus@ushunger.com>
2026-07-08 16:17:25 +0000

    React frontend with tailwind initialization

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-black w-full flex flex-1">
+      <p>HI</p>
+    </div>
+  )
+}
+
+export default App

68018671d69f728a90001ce97a294fe64f7e0d5e
unknown <jayden.aurelus@ushunger.com>
2026-07-20 18:05:36 +0000

    Coors + backend-frontend communication

--- spGenerator/spGenerator/AppStart/WebApiConfig.cs
+++ spGenerator/spGenerator/AppStart/WebApiConfig.cs
@@ -1,12 +1,25 @@
 using System.Web.Http;
-namespace spGenerator{
+using System.Web.UI.WebControls;
+namespace spGenerator
+{
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
             config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling =
-    Newtonsoft.Json.ReferenceLoopHandling.Ignore;  
+    Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            // Enable CORS
+            var cors = new System.Web.Http.Cors.EnableCorsAttribute(
+                origins: "http://localhost:3000",
+                headers: "*",
+                methods: "*");
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = System.Web.Http.RouteParameter.Optional }
+            );
+
         }
     }
 }

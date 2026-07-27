705d45bfba40960954a47ff158169da567ea6548
unknown <jayden.aurelus@ushunger.com>
2026-07-27 21:16:23 +0000

    Image view

--- frontend/src/lib/routes.tsx
+++ frontend/src/lib/routes.tsx
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import App from '../App'
 import Past from '../Past'
 import Request from '../Request'
+import ImagePage from '../image'
+
 
 const AppRoutes: React.FC = () => (
     <Routes>
         <Route path='/' element={<App />} />
         < Route path='/past' element={<Past />} />
         < Route path='/requests/:id' element={<Request />} />
+        < Route path='/requests/:id/image' element={<ImagePage />} />
+
 
     </Routes>
 );
